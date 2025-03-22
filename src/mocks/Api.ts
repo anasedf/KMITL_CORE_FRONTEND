caa34346cc421a10fe3387fe1ce5a53643c084d6
anasedf <anasandfred@gmail.com>
2025-03-23 05:07:41 +0700

    ลบไฟล์ไม่จำเป็น

--- src/mocks/Api.ts
+++
@@ -1,4 +0,0 @@
-// src/apiEndpoints.ts
-export const COURSES_API_URL = 'https://course-review-back-app-741869911637.asia-southeast1.run.app/api/courses/';
-export const REVIEWS_API_URL = 'https://course-review-back-app-741869911637.asia-southeast1.run.app/api/reviews/';
-export const QUESTIONS_API_URL = 'https://course-review-back-app-741869911637.asia-southeast1.run.app/api/questions/';
