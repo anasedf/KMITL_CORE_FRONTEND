9a7e12e37fb49e9360c43e1c066fcbdf11dcba58
yimsarasorr <yimsara000@gmail.com>
2025-03-26 00:12:52 +0700

    Update Footer.tsx

--- src/Component/Nav/Footer.tsx
+++ src/Component/Nav/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
       <footer>
-        <p>Contact us: info@kmitlclap.com</p>
-        <p>&copy; 2023 KMITLCLAP. All rights reserved.</p>
+        <p>Contact us: kmitlcore</p>
+        <p>&copy; 2023 KMITLCORE. All rights reserved.</p>
       </footer>
     );
   };
