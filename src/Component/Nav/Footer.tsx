e6c9418a518490a9843f9d519b66ceea017b3f9f
yimsarasorr <yimsara000@gmail.com>
2025-03-26 00:13:48 +0700

    แก้  footer

--- src/Component/Nav/Footer.tsx
+++ src/Component/Nav/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer>
         <p>Contact us: kmitlcore</p>
-        <p>&copy; 2023 KMITLCORE. All rights reserved.</p>
+        <p>&copy; KMITLCORE. All rights reserved.</p>
       </footer>
     );
   };
